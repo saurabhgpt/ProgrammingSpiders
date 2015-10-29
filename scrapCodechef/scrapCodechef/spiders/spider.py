f14c622c7ca1b92751494d2b997bc8556f7c39e0
lethalgit <saurabhz@outlook.com>
2015-10-29 23:43:52 +0530

    Modified Codechef webCrawler bahaviour, added some more attributes

--- scrapCodechef/scrapCodechef/spiders/spider.py
+++ scrapCodechef/scrapCodechef/spiders/spider.py
@@ -17,14 +17,15 @@ class scrapCodechefSpider(scrapy.Spider) :
 		"https://www.codechef.com/problems/extcontest"
 	]
 
+
 	def parse(self, response) :
 		browser = webdriver.Firefox()
 		browser.get(response.url)
 		time.sleep(15)
 		platform = "Codechef"
-		with open("/home/lethal/PYTHON/PythonEnv/venv/.git/ProgrammingSpiders/scrapCodechef/scrapCodechef/Data/scrapCodechef.csv","ab+") as f:
+		with open("/home/lethal/PYTHON/PythonEnv/venv/git/ProgrammingSpiders/scrapCodechef/scrapCodechef/Data/scrapCodechef.csv","ab+") as f:
 			writer = csv.writer(f)
-			headers = ["Name", "Slug", "Submissions", "Accuracy", "QuestionURL", "Platform"]
+			headers = ["Name", "Slug", "Submissions", "Accuracy", "QuestionURL", "Platform", "Tags", "LanguagesAllowed", "DateAdded"]
 			writer.writerow(headers)
 			for sel in response.xpath("//*[@id='primary-content']/div/div/div[2]/table/tbody/tr") :
 				if(len(str(sel.xpath('td[1]/div[1]/a/b/text()').extract())) > 2) :
@@ -38,24 +39,12 @@ class scrapCodechefSpider(scrapy.Spider) :
 					submissions = submissions.encode('utf8')
 					accuracy = accuracy.encode('utf8')
 					questionURL = questionURL.encode('utf8')
-					questionDetailObject = [name, slug, submissions, accuracy, questionURL, platform]
+					tags = ""
+					languagesAllowed = ""
+					dateAdded = ""
+					questionDetailObject = [name, slug, submissions, accuracy, questionURL, platform, tags, languagesAllowed, dateAdded]
+					print "HELLO HERE............................"
+					print questionDetailObject
 					writer.writerow(questionDetailObject.encode('utf8') if type(questionDetailObject) is unicode else questionDetailObject)
 		browser.quit()
 		f.close()
-
-	def parseQuestionDetails(self, item, response) :
-		# print "YEYYEY"
-		item = response.meta['item']
-		item = self.getMoreInfo(item, response)
-		return item
-
-	def getMoreInfo(self, item, response) :
-		f_out.write("tags: ")
-		f_out.write(str(response.xpath("//*[@id='problem-page']/div/div/div[1]/div[@id='node-4574316']/table/tbody/tr[4]/td[2]/a/text()").extract()))
-		f_out.write(", ")
-		f_out.write("dateAdded: ")
-		f_out.write(str(response.xpath("//*[@id='problem-page']/div/div/div[1]/div[@id='node-4574316']/table/tbody/tr[5]/td[2]/text()").extract()))
-		f_out.write(", ")
-		f_out.write("LanguagesAllowed: ")
-		item["LanguagesAllowed"] = f_out.write(str(response.xpath("//*[@id='problem-page']/div/div/div[1]/div[@id='node-4574316']/table/tbody/tr[8]/td[2]/text()").extract()))
-		f_out.write("\n")
